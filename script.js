e0de9a62277ff5e02d7ed9f86e5db13b319bae94
RBULIIII <buli9990@student.acitech.org>
2025-03-03 09:12:38 -0500

    I added text alert. I need to add animations and movement. Everything works so far. Some code like my text didnt work at first but know it does

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-const button1 = document.getElementById("button1");
+let text = document.getElementById("text")
+let button1 = document.getElementById("button1")
 
-button1.addEventListener("click" , yell_at_user);
+button1.addEventListener("click" , annoy);
 
 function yell_at_user() {
     alert("STOP PRESSING THIS BUTTON!");
+
 }
+
+let clicks= 0;
+
+function annoy (){
+    clicks ++;
+    console.log(clicks)
+    if( clicks == 1 ) {
+text.innerHTML = "HEY CUTT IT OUTT!!";
+    }
+    else  if( clicks == 5 ) {
+        text.innerHTML = "I SAID STOPPP!";
+            }
+    else  if( clicks == 10 ) {
+        text.innerHTML = "YOU WILL REGRET ANOTHER PRESS!!";
+            }
+    else  if( clicks == 15 ) {
+        text.innerHTML = "YOU ARE STARTING TO MAKE ME MAD";
+            }
+    else  if( clicks == 20 ) {
+        text.innerHTML = "If you stop I'll give you a reward";
+            }
+    else  if( clicks == 25 ) {
+        text.innerHTML = "Okay... catch me if you can.";
+            }
+}
+
