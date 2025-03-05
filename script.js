bacd4a3172ed431b154a89b75e9c38e54ba44a4e
RBULIIII <buli9990@student.acitech.org>
2025-03-05 09:09:57 -0500

    I added movement to my button and added a second button.Everything works I had trouble setting up my second button at first.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let text = document.getElementById("text")
 let button1 = document.getElementById("button1")
 
+
 button1.addEventListener("click" , annoy);
 
 function yell_at_user() {
@@ -33,7 +34,44 @@ text.innerHTML = "HEY CUTT IT OUTT!!";
         button1.classList.remove("mainDiv");
             }
     else  if( clicks == 25 ) {
-        text.innerHTML = "Okay... catch me if you can.";
+        text.innerHTML = "Okay... catch me if you can. >:)";
             }
+    else  if( clicks == 30 ) {
+        button1.classList.add ("movement");
+            }   
+    else  if( clicks == 33 ) {
+        text.innerHTML = "HEHEHE >:)";
+        button1.classList.add ("movement2");
+            }
+            
+    else  if( clicks == 25 ) {
+        text.innerHTML = "CANT GET ME >:)";
+        button1.classList.add ("movement3");
+            }
+            
+    else  if( clicks == 37 ) {
+        text.innerHTML = "YOU DONE ? >:)";
+        button1.classList.add ("movement4");
+            }
+            
+    else  if( clicks == 39 ) {
+        text.innerHTML = "YOU WILL NEVER GET ME >:)";
+        button1.classList.add ("movement5");
+            }
+    else  if( clicks == 40 ) {
+        text.innerHTML = "YOU STILL PRESSING ME >:(";
+        button1.classList.remove ("movement5");
+        button1.classList.remove ("movement4");
+        button1.classList.remove ("movement3");
+        button1.classList.remove ("movement2");
+        button1.classList.remove ("movement");
+            }
+    else  if( clicks == 43) {
+        text.innerHTML = "How about you press him instead";
+        const button2 = document.createElement("button");
+        button2.innerHTML = "PRESS MEEEEEE";
+        button2.classList.add("button1");
+        button2.classList.add ("button2")
+        container.appendChild(button2);
 }
-
+}
