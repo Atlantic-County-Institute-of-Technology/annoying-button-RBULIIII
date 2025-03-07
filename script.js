7832382fa9fb36ae2d16ea137f3558dc2495bb66
RBULIIII <buli9990@student.acitech.org>
2025-03-07 09:15:56 -0500

    I FINISHED ADDING ANIMATION AND ADDING THINGS TO MY button. I also just need to add a final animation. I added a new sound. Everything works so far.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let text = document.getElementById("text")
 let button1 = document.getElementById("button1")
-/*let sound = new Audio('assests/sound.mp3')*/
-let sigma = new Audio('assests/sigma.mp3')
+let mouse = new Audio('assests/mouse.mp3')
 
 button1.addEventListener("click" , annoy);
 
@@ -13,7 +12,7 @@ function yell_at_user() {
 let clicks= 0;
 
 function annoy (){
-    sigma.play();
+    mouse.play();
     clicks ++;
     console.log(clicks)
     if( clicks == 1 ) {
@@ -28,12 +27,10 @@ text.innerHTML = "HEY CUTT IT OUTT!!";
     else  if( clicks == 15 ) {
         text.innerHTML = "YOU ARE STARTING TO MAKE ME MAD";
         button1.classList.add("angry");
-        button1.classList.add("mainDiv");
             }
     else  if( clicks == 20 ) {
         text.innerHTML = "If you stop I'll give you a reward";
         button1.classList.remove("angry");
-        button1.classList.remove("mainDiv");
             }
     else  if( clicks == 25 ) {
         text.innerHTML = "Okay... catch me if you can. >:)";
@@ -69,15 +66,36 @@ text.innerHTML = "HEY CUTT IT OUTT!!";
         button1.classList.remove ("movement");
             }
     else  if( clicks == 43) {
-        text.innerHTML = "How about you press him instead";
-        const button2 = document.createElement("button");
-        button2.innerHTML = "PRESS MEEEEEE";
-        button2.classList.add("button1");
-        button2.classList.add ("button2")
-        container.appendChild(button2);
-}
-    else if(clicks == 50){
-        button1.classList.add ("glitch")
+        text.innerHTML = "Okay.... TRY AND PRESS ME NOW >:)";
+        button1.classList.add("glitch");
     }
 
+    else  if( clicks == 44) {
+        text.innerHTML = "STOP PRESSSING MEEE";
+        button1.classList.remove("glitch");
+    }
+    else if(clicks == 50){ button1.classList.remove("glitch"); 
+    text.innerHTML = "OKAY FINE PRESS HIM THEN"; const button2 = document.createElement("button"); 
+    button2.innerHTML = "PRESS MEEEEEE"; 
+    button2.classList.add("button1"); 
+    button2.classList.add ("button2"); 
+    container.appendChild(button2); 
+    } 
+
+    else  if( clicks == 67) { 
+        text.innerHTML = "WHY DO YOU KEEP PRESSING ME"; 
+    }
+
+    else  if( clicks == 70) { 
+        text.innerHTML = "IF YOU HAVENT FIGURED IT OUT BY NOW THE GREEN BUTTON DOES NOTHING LOL"; 
+    }
+
+    else  if( clicks == 76) { 
+        text.innerHTML = "JUST WAIT UNTIL 100"; 
+    }
+
+    else if(clicks == 100 ){
+        text.innerHTML = "ALRIGHT YOU MADE ME DO THIS MUAHAHHA >:)";
+        
+    }
 }
